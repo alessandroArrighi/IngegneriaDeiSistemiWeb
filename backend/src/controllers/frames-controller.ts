f61f87519aa31e8351076f29166fb3fa8798d0b9
alessandroArrighi <alessandro.arrighi2@studio.unibo.it>
2023-11-29 19:31:14 +0100

    create frames-controller.ts

---
+++ backend/src/controllers/frames-controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from 'express'
+import { connection } from '../utils/db'
+
+export async function allFrames(req: Request, res: Response) {
+    connection.execute(
+        ``
+    )
+}
