f8d457bfdc07c947d4c3549e805a43630f99d267
alessandroArrighi <alessandro.arrighi2@studio.unibo.it>
2023-11-29 19:34:03 +0100

    finish frames-controller.ts

--- backend/src/controllers/frames-controller.ts
+++ backend/src/controllers/frames-controller.ts
@@ -3,6 +3,10 @@ import { connection } from '../utils/db'
 
 export async function allFrames(req: Request, res: Response) {
     connection.execute(
-        ``
+        `SELECT * FROM Montature`,
+        [],
+        function(err, results, fields) {
+            res.json(results)
+        }
     )
 }
