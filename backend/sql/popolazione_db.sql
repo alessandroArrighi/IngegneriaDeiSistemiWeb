e81c251529bddcdccaf5a8c87c171dec141b0e5a
alessandroArrighi <alessandro.arrighi2@studio.unibo.it>
2023-11-29 20:03:24 +0100

    create popolazione_db.sql: insert dates in Montature table

---
+++ backend/sql/popolazione_db.sql
@@ -0,0 +1,15 @@
+USE `Progetto`;
+INSERT INTO Montature (Modello, Brand, IDProdotto, Prezzo, Versione, Calibro, Ponte, Aste, Materiale, Colore)
+VALUES ('CCC00', 'RayBan', '00C00', 180, 'da vista', 52, 16, 135, 'metallo', 'bianco'),
+('CCC01', 'EA', '00C01', 250, 'da vista', 58, 22, 142, 'acetato', 'rosso'),
+('CCC02', 'Vogue', '00C02', 220, 'da sole', 60, 18, 140, 'composito', 'verde'),
+('CCC03', 'Gucci', '00C03', 150, 'da vista', 55, 20, 138, 'metallo', 'blu'),
+('CCC04', 'SL', '00C04', 200, 'da vista', 58, 22, 145, 'acetato', 'giallo'),
+('CCC05', 'BV', '00C05', 280, 'da sole', 62, 21, 142, 'composito', 'rosa'),
+('CCC06', 'Dior', '00C06', 190, 'da vista', 54, 17, 140, 'metallo', 'grigio'),
+('CCC07', 'Tiffany', '00C07', 150, 'da vista', 57, 19, 140, 'acetato', 'oro'),
+('CCC08', 'D&G', '00C08', 220, 'da vista', 59, 23, 140, 'composito', 'argento'),
+('CCC09', 'Persol', '00C09', 200, 'da vista', 58, 22, 142, 'metallo', 'fucsia'),
+('CCC10', 'Tom Ford', '00C10', 180, 'da sole', 50, 15, 135, 'acetato', 'azzurro'),
+('CCC11', 'GA', '00C11', 240, 'da vista', 56, 21, 138, 'composito', 'marrone'),
+('CCC12', 'GA', '00C12', 260, 'da vista', 61, 24, 142, 'metallo', 'bianco');
