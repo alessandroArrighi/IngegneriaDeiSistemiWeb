3e24450b5bc0924e1f319682e41a680fd8ef4987
alessandroArrighi <alessandro.arrighi2@studio.unibo.it>
2023-12-02 12:50:37 +0100

    insert in popolazione_db in Montature table all Immagine instances

--- backend/sql/popolazione_db.sql
+++ backend/sql/popolazione_db.sql
@@ -2,17 +2,17 @@ USE `Progetto`;
 INSERT INTO Montature (Modello, Brand, IDProdotto, Prezzo, Versione, Calibro, Ponte, Aste, Materiale, Colore, Immagine)
 VALUES ('CCC00', 'RayBan', '00C00', 180, 'da vista', 52, 16, 135, 'metallo', 'bianco', '/immagini/eyeglasses-wear.jpg'),
 ('CCC01', 'EA', '00C01', 250, 'da vista', 58, 22, 142, 'acetato', 'rosso', 'immagini/sunglasses.jpg'),
-('CCC02', 'Vogue', '00C02', 220, 'da sole', 60, 18, 140, 'composito', 'verde'),
-('CCC03', 'Gucci', '00C03', 150, 'da vista', 55, 20, 138, 'metallo', 'blu'),
-('CCC04', 'SL', '00C04', 200, 'da vista', 58, 22, 145, 'acetato', 'giallo'),
-('CCC05', 'BV', '00C05', 280, 'da sole', 62, 21, 142, 'composito', 'rosa'),
-('CCC06', 'Dior', '00C06', 190, 'da vista', 54, 17, 140, 'metallo', 'grigio'),
-('CCC07', 'Tiffany', '00C07', 150, 'da vista', 57, 19, 140, 'acetato', 'oro'),
-('CCC08', 'D&G', '00C08', 220, 'da vista', 59, 23, 140, 'composito', 'argento'),
-('CCC09', 'Persol', '00C09', 200, 'da vista', 58, 22, 142, 'metallo', 'fucsia'),
-('CCC10', 'Tom Ford', '00C10', 180, 'da sole', 50, 15, 135, 'acetato', 'azzurro'),
-('CCC11', 'GA', '00C11', 240, 'da vista', 56, 21, 138, 'composito', 'marrone'),
-('CCC12', 'GA', '00C12', 260, 'da vista', 61, 24, 142, 'metallo', 'bianco');
+('CCC02', 'Vogue', '00C02', 220, 'da sole', 60, 18, 140, 'composito', 'verde', '/immagini/eyeglasses-wear.jpg'),
+('CCC03', 'Gucci', '00C03', 150, 'da vista', 55, 20, 138, 'metallo', 'blu', '/immagini/eyeglasses-wear.jpg'),
+('CCC04', 'SL', '00C04', 200, 'da vista', 58, 22, 145, 'acetato', 'giallo', '/immagini/eyeglasses-wear.jpg'),
+('CCC05', 'BV', '00C05', 280, 'da sole', 62, 21, 142, 'composito', 'rosa', '/immagini/eyeglasses-wear.jpg'),
+('CCC06', 'Dior', '00C06', 190, 'da vista', 54, 17, 140, 'metallo', 'grigio', '/immagini/eyeglasses-wear.jpg'),
+('CCC07', 'Tiffany', '00C07', 150, 'da vista', 57, 19, 140, 'acetato', 'oro', '/immagini/eyeglasses-wear.jpg'),
+('CCC08', 'D&G', '00C08', 220, 'da vista', 59, 23, 140, 'composito', 'argento', '/immagini/eyeglasses-wear.jpg'),
+('CCC09', 'Persol', '00C09', 200, 'da vista', 58, 22, 142, 'metallo', 'fucsia', '/immagini/eyeglasses-wear.jpg'),
+('CCC10', 'Tom Ford', '00C10', 180, 'da sole', 50, 15, 135, 'acetato', 'azzurro', '/immagini/eyeglasses-wear.jpg'),
+('CCC11', 'GA', '00C11', 240, 'da vista', 56, 21, 138, 'composito', 'marrone', '/immagini/eyeglasses-wear.jpg'),
+('CCC12', 'GA', '00C12', 260, 'da vista', 61, 24, 142, 'metallo', 'bianco', '/immagini/eyeglasses-wear.jpg');
 
 INSERT INTO Lenti (Modello, Brand, IDProdotto, Prezzo, Versione, Antiriflesso, Sfericità, Focale)
 VALUES ('BBB00', 'Shamir', '00B00', 70, 'graduata', false, 'sferica', 'monofocale'),
