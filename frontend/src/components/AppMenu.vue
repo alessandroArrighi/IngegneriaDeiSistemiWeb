403196c053193aaae1b103f15ec50e9d8bc80331
RiccardoM <riccardomedri9@gmail.com>
2023-12-02 11:54:40 +0100

    slight improvemenet of navbar

--- frontend/src/components/AppMenu.vue
+++ frontend/src/components/AppMenu.vue
@@ -1,24 +1,24 @@
 <template>
     <div class="flex-container">
-            <nav>
-                <div class="flex-item">
-                    <button @click="toggleFlexItem" class="toggle-button">Toggle</button>
-                </div>
-                <div class="flex-item">
-                    <a href="/">
-                        <img src="public/tumefatti-eyewear-high-resolution-logo-transparent.png" width="130" height="100" alt="logo">
-                        <h1>Tumefatti</h1>
-                    </a>
-                </div>
-                <div class="flex-item" v-if="showFlexItem">
-                    <ul>
-                        <li><RouterLink to = "/">Pagina 1</RouterLink></li>
-                        <li><RouterLink to = "/">Pagina 2</RouterLink></li>
-                        <li><RouterLink to = "/">Pagina 3</RouterLink></li>
-                        <li><RouterLink to = "/">Pagina 4</RouterLink></li>
-                    </ul>
-                </div>
-            </nav>
+        <nav>
+            <div class="flex-item">
+                <button @click="toggleFlexItem" class="toggle-button">Toggle</button>
+            </div>
+            <div class="flex-item">
+                <a href="/">
+                    <img src="public/tumefatti-eyewear-high-resolution-logo-transparent.png" width="130" height="100" alt="logo">
+                    <h1>Tumefatti</h1>
+                </a>
+            </div>
+        </nav>
+        <div class="flex-item" id="menu" v-if="showFlexItem">
+            <ul>
+                <li><RouterLink to = "/">Pagina 1</RouterLink></li>
+                <li><RouterLink to = "/">Pagina 2</RouterLink></li>
+                <li><RouterLink to = "/">Pagina 3</RouterLink></li>
+                <li><RouterLink to = "/">Pagina 4</RouterLink></li>
+            </ul>
+        </div>
     </div>
 </template>
 
