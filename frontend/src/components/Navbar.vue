724df33d3f72a30f7c67b2271cdb1f0e4de968aa
RiccardoM <riccardomedri9@gmail.com>
2024-02-06 22:06:00 +0100

    frontend 6

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -41,12 +41,19 @@ export default defineComponent({
             <div class="collapse navbar-collapse justify-content-end fw-bold" id="navbarNavAltMarkup">
                 <div class="navbar-nav pb-1 me-5">
                     <ul>
-                        <li><RouterLink class="nav-link active text-white px-3 py-2 mt-2 me-2 fs-5" aria-current="page" to = "/">Home</RouterLink></li>
+                        <!--<li><RouterLink class="nav-link active text-white px-3 py-2 mt-2 me-2 fs-5" aria-current="page" to = "/">Home</RouterLink></li>-->  
                         <li><RouterLink class="nav-link active text-white px-3 py-2 mt-2 me-2 fs-5" to = "/prodotti/tutti/vista/MontatureV">Occhiali da Vista</RouterLink></li>
                         <li><RouterLink class="nav-link active text-white px-3 py-2 mt-2 me-2 fs-5" to = "/prodotti/tutti/sole/MontatureS">Occhiali da Sole</RouterLink></li>
                         <li><RouterLink class="nav-link active text-white px-3 py-2 mt-2 me-2 fs-5" to = "/prodotti/tutti/lac/LACC">Lenti a Contatto</RouterLink></li>
                         <li v-if="!user"><RouterLink class="nav-link text-warning px-3 py-2 mt-2 me-2 fs-5" to = "/accesso/user">Accedi</RouterLink></li>
                         <li v-if="user"><RouterLink class="nav-link text-warning px-3 py-2 mt-2 me-2 fs-5" to ="/accesso/areaPersonale">Area Personale</RouterLink></li>
+                        <li v-if="user">
+                            <RouterLink class="nav-link text-warning px-3 py-2 mt-2 me-2 fs-5" to ="/accesso/areaPersonale">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+                                </svg>
+                            </RouterLink>
+                        </li>
                     </ul>
                 </div>
             </div>                       
